0226e824c1aba2ebb78b9e94ed8d30ed881a7030
sharon <sharon.mayenkar01@gmail.com>
2015-07-09 17:21:49 +0530

    Final commit

--- 1316Onlinetest1/src/EVM.java
+++ 1316Onlinetest1/src/EVM.java
@@ -5,6 +5,8 @@ readyLamp readylampOn=new readyLamp();
 ballotUnit ballotUnit=new ballotUnit();
 candidateButton candidateButton=new candidateButton();
 totalButton totalButton=new totalButton();
+resultButton resultButton=new resultButton();
+
 public void startEVM(){
 	System.out.println("EVM turned on");
 	readylampOn.setLampState("Glowing");
@@ -14,11 +16,8 @@ public void startEVM(){
 	ballotUnit.relaseBallot();
 	candidateButton.castYourVote();
 	System.out.println("Total Voters till now : "+totalButton.getTotalVotes());
-	System.out.println("/nvotes for individuals");
-	System.out.println("Contestant Alisha : "+candidateButton.ali2);
-	System.out.println("Contestant Nazeef : "+candidateButton.naz3);
-	System.out.println("Contestant Pratiksha : "+candidateButton.pra1);
-	System.out.println("Contestant Bindiya : "+candidateButton.bin4);
+	resultButton.getVotesForIndividual();
+	resultButton.declareWinner();
 }
 
 public void stopEVM(){
--- 1316Onlinetest1/src/candidateButton.java
+++ 1316Onlinetest1/src/candidateButton.java
@@ -2,10 +2,11 @@
 public class candidateButton extends ballotButton {
 //pressed by voter
 	int pra1=10,ali2=11,naz3=34,bin4=23;
-	public void castYourVote(){
+	public int castYourVote(){
 		ballotUnit ballotUnit=new ballotUnit();
 	   //select your choice
 		System.out.println("Cast your vote");
 		pra1=pra1+1;
+		return pra1;
 	}
 }
